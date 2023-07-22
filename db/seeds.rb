23f0c7d9ac7a3c6786d7490cd73db0be64ba9416
fukumochi-0110 <fukumoto08@gmail.com>
2023-07-22 23:34:52 +0900

    日付データを生成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-start_date = Date.new(2020, 1, 1)
-end_date = Date.new(2050, 12, 31)
+start_date = Date.new(2023, 1, 1)
+end_date = Date.new(2033, 12, 31)
 
 (start_date..end_date).each do |date|
   EventDate.create(event_date: date)  
