a630a6859d19d1afb11110c62ec4de724ad15aec
fukumochi-0110 <fukumoto08@gmail.com>
2023-07-27 00:27:11 +0900

    taskコントローラ作成

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,7 @@
 class Task < ApplicationRecord
+  validates :text, :name, :due_date, :event_date_id, :unit_id, presence: true
+  validates :completed, inclusion: { in: [true, false] }
+
+  belongs_to :unit
+  belongs_to :event_date
 end
