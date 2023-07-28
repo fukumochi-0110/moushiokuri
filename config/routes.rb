3f8d3dbacb25638f693c1825d42f03190dddf12f
fukumochi-0110 <fukumoto08@gmail.com>
2023-07-29 00:57:37 +0900

    ルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   end
 
   resources :messages, only: [:create, :edit, :update, :destroy]
-  resources :tasks, only: [:create]
+  resources :tasks, only: [:create, :edit, :update,]
 
 end
