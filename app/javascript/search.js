a3892f9097f3677663a4ddb84b181b78eaf1e586
fukumochi-0110 <fukumoto08@gmail.com>
2023-08-20 01:58:19 +0900

    search.jsを修正画面が出るようにした

--- app/javascript/search.js
+++ app/javascript/search.js
@@ -1,70 +1,70 @@
-function search (){
+function search() {
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
-  e.preventDefault();
-  const form = document.getElementById("form");
-  const formData = new FormData(form);
-  const XHR = new XMLHttpRequest();
-  const query = form.q.value; 
-  XHR.open("GET", "/search?q=" + query, true); 
-  XHR.responseType = "json";
-  XHR.send(formData);
-  XHR.onload = () => {
-    const wrapperContainer = document.getElementById("wrapper-container")
-    let searchResultsHtml = '';
+    e.preventDefault();
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    const query = form.q.value;
+
+    if (query.trim() === "") {
+      const searchResultTable = document.getElementById('searchResultTable');
+      searchResultTable.innerHTML = "一致する結果がありません";
+      return;
+    }
+
+    XHR.open("GET", "/search?q=" + query, true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+    XHR.onload = () => {
+      const wrapperContainer = document.getElementById("wrapper-container");
+      let searchResultsHtml = '';
+      const searchResultTable = document.getElementById('searchResultTable');
+      const searchResultOverlay = document.getElementById('searchResultOverlay');
+
+      window.addEventListener('click', (e) => {
+        const searchResultOverlay = document.getElementById('searchResultOverlay');
+        if (e.target !== searchResultOverlay) {
+          searchResultOverlay.style.display = 'none';
+        }
+      });
+
+      if (searchResultOverlay) {
+        searchResultOverlay.style.display = 'block';
+      }
 
     const MessagesSearches = XHR.response.messages_search;
+
+    if (MessagesSearches.length === 0) {
+      searchResultTable.innerHTML = "一致する結果がありません";
+      return;
+    }
+
     MessagesSearches.forEach((message) => {
-      const createdAt = new Date(message.created_at); 
-      const formattedDate = `${createdAt.getMonth() + 1}/${createdAt.getDate()}`; 
-    
-      const Moushiokuri = `
-      <div class="message-content">
-        <div class="moushiokuri">
-          <div class="moushiokuri-title">
-            <div>${formattedDate} 申し送り</div>
-          </div>
-          <div class="moushiokuri-text">
-            ${ message.text }
-          </div>
-          <div class="moushiokuri-image">
-            ${ message.image ? `<img src="${message.image}" class="message-image">` : '' }
-          </div>
-          <div class="moushiokuri-under">
-            ${ message.name }
-          </div>
-        </div>
-      </div>`;
-      searchResultsHtml += Moushiokuri;
-    });
-    
-    const TasksSearches = XHR.response.tasks_search;
-    TasksSearches.forEach((task) => {
-      const dueDate = new Date(task.due_date);
-      const formattedDueDate = `${dueDate.getMonth() + 1}/${dueDate.getDate()} ${dueDate.getHours()}:${dueDate.getMinutes()}`;
-    
-      const taskHtml = `
-      <div class="message-content">
-        <div id="non-comp">
-          <div class="comp-title">タスク</div>
-          <div class="not-comp">
-            <div class="task-text">${ task.text }</div>
-            <div class="task-under">
-              ${ task.name }
-              期限：${ formattedDueDate }
-              <form action="/path/to/your/endpoint" id="form-${task.id}">
-                <input type="checkbox" name="completed" id="check-${task.id}" ${task.completed ? 'checked' : ''}>
-              </form>
+      const createdAt = new Date(message.created_at);
+      const formattedDate = `${createdAt.getFullYear()}年  ${createdAt.getMonth() + 1}月${createdAt.getDate()}日`;
+
+      searchResultsHtml += `
+        <div class="message-content">
+          <div class="moushiokuri">
+            <div class="moushiokuri-title">
+              <div>${formattedDate} 申し送り</div>
+            </div>
+            <div class="moushiokuri-text">
+              ${message.text}
+            </div>
+            <div class="moushiokuri-image">
+              ${message.image ? `<img src="${message.image}" class="message-image">` : ''}
+            </div>
+            <div class="moushiokuri-under">
+              ${message.name}
             </div>
           </div>
-        </div>
-      </div>`;
-      searchResultsHtml += taskHtml;
+        </div>`;
+      });
 
-    });
-    wrapperContainer.insertAdjacentHTML("beforeend", searchResultsHtml);
-      };
-    });
-  };
-  
-  window.addEventListener('load', search);
+      searchResultTable.innerHTML = searchResultsHtml;
+    };
+  });
+}
+window.addEventListener('load', search);
